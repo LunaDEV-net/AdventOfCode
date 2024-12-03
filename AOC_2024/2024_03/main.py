2c56519173dc0faf91d4b48adda7b60cea337f43
luna <93258461+LunaDEV-net@users.noreply.github.com>
2024-12-03 10:01:44 +0100

    Update main.py

--- AOC_2024/2024_03/main.py
+++ AOC_2024/2024_03/main.py
@@ -4,6 +4,7 @@ txt = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 hi = "mul(11,13)"
 
 def main(inp):
+    ergebnis = 0
     data = []
     q = r"mul\(\d+,\d+\)"
     all_mul = re.findall(q, inp)
@@ -12,7 +13,10 @@ def main(inp):
         left = int("".join(re.findall(r"\d", mul[0])))
         right = int("".join(re.findall(r"\d", mul[1])))
         data.append([left,right])
-        print(data)
+    print(data)
+    for multi in data:
+        ergebnis += multi[0]*multi[1]
+    print(ergebnis)
 
 if __name__ == "__main__":
     print("hi")
