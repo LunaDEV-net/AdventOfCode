5c98fd20fa3767a88872ce105f11d0cc4ace3ab3
luna <93258461+LunaDEV-net@users.noreply.github.com>
2024-12-03 09:46:01 +0100

    Update main.py

--- AOC_2024/2024_03/main.py
+++ AOC_2024/2024_03/main.py
@@ -3,10 +3,14 @@ import re
 txt = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
 def main(inp):
+    data = []
     q = r"mul\(\d+,\d+\)"
     all_mul = re.findall(q, inp)
     for mul in all_mul:
         mul = re.split(r",", mul)
+        left = re.findall(r"\d", mul[0])
+        print(left)
+        data.append([left])
         print(mul)
 
 if __name__ == "__main__":
