798e7fba386464508aacf117811294b336819c20
luna <93258461+LunaDEV-net@users.noreply.github.com>
2024-12-03 09:34:31 +0100

    fix code

--- AOC_2024/2024_03/main.py
+++ AOC_2024/2024_03/main.py
@@ -3,11 +3,11 @@ import re
 txt = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 
 def main(inp):
-  q = "mul\([0-9]+?,[0-9]+?\)/g"
-  all_mul = re.findall(q, inp)
-  for mul in all_mul:
-    print(mul)
+    q = r"mul\(\d+,\d+\)"
+    all_mul = re.findall(q, inp)
+    for mul in all_mul:
+        print(mul)
 
 if __name__ == "__main__":
-  print("hi")
-  main(txt)
+    print("hi")
+    main(txt)
