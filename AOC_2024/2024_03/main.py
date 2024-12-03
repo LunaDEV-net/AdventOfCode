39a99d651073ea7fd3580961423ddf954ca0bb1f
luna <93258461+LunaDEV-net@users.noreply.github.com>
2024-12-03 09:49:37 +0100

    Update main.py

--- AOC_2024/2024_03/main.py
+++ AOC_2024/2024_03/main.py
@@ -1,6 +1,7 @@
 import re
 
 txt = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+hi = "mul(11,13)"
 
 def main(inp):
     data = []
@@ -16,4 +17,4 @@ def main(inp):
 
 if __name__ == "__main__":
     print("hi")
-    main(txt)
+    main(hi)
