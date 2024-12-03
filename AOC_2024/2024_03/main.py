e1a6535821dbd0f3c5ef5d2d04ba06f85b029c99
luna <93258461+LunaDEV-net@users.noreply.github.com>
2024-12-03 10:07:23 +0100

    Update main.py

--- AOC_2024/2024_03/main.py
+++ AOC_2024/2024_03/main.py
@@ -3,6 +3,11 @@ import re
 txt = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
 hi = "mul(11,13)"
 
+
+def load_input_file(path="input.txt"):
+    with open(path) as f:
+        return f.read()
+
 def main(inp):
     ergebnis = 0
     data = []
@@ -20,4 +25,4 @@ def main(inp):
 
 if __name__ == "__main__":
     print("hi")
-    main(txt)
+    main(load_input_file())
